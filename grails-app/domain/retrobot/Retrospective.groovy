8aed73425c476fa6460b32549931c003d7d37ffe
stefankendall <stefankendall@gmail.com>
2012-12-13 17:05:28 -0500

    added ability to create a retrospective

--- grails-app/domain/retrobot/Retrospective.groovy
+++ grails-app/domain/retrobot/Retrospective.groovy
@@ -2,7 +2,9 @@ package retrobot
 
 class Retrospective {
 
-
+    List discussionItems
+    static hasMany = [discussionItems: DiscussionItem]
+    Boolean isActive
 
     static constraints = {
     }
---
+++ test/unit/retrobot/DiscussionItemTests.groovy
@@ -0,0 +1,17 @@
+package retrobot
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(DiscussionItem)
+class DiscussionItemTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
