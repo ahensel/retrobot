bcc00b8c1d73704e51e3b8e6b0503e4bcdb724e6
Alan Hensel and Matt McClellan <pair+ahensel+mmcclellan@rallydev.com>
2012-12-13 17:05:28 -0500

    added ability to create a retrospective

---
+++ grails-app/domain/retrobot/DiscussionItem.groovy
@@ -0,0 +1,12 @@
+package retrobot
+
+class DiscussionItem {
+
+    String content
+
+    static belongsTo = [retrospective: Retrospective]
+
+    static constraints = {
+        content nullable: false
+    }
+}
---
+++ grails-app/views/retrospective/_form.gsp
@@ -0,0 +1,4 @@
+<%@ page import="retrobot.Retrospective" %>
+
+
+
