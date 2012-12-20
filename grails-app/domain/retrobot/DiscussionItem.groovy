85e3b45ee14a612aa121ce8cb0565a8062fbd6d6
Alan Hensel and Matt McClellan <pair+ahensel+mmcclellan@rallydev.com>
2012-12-20 10:47:03 -0500

    added discussion item numbering, and changed default page font to Helvetica

--- grails-app/domain/retrobot/DiscussionItem.groovy
+++ grails-app/domain/retrobot/DiscussionItem.groovy
@@ -5,6 +5,7 @@ class DiscussionItem {
     String content
     static hasMany = [actionItems: ActionItem]
     static belongsTo = [retrospective: Retrospective]
+    int number
 
     static constraints = {
         content nullable: false
--- grails-app/services/retrobot/DiscussionItemService.groovy
+++ grails-app/services/retrobot/DiscussionItemService.groovy
@@ -8,8 +8,14 @@ class DiscussionItemService {
         if(retro){
             def item = new DiscussionItem(content: content)
             retro.addToDiscussionItems(item)
+            item.number = findHighestDiscussionItemNumberInRetro(retro) + 1
             retro.save()
             return item
         }
+    }
+
+    private int findHighestDiscussionItemNumberInRetro(Retrospective retro) {
+        // race condition - need better way in the long run
+        retro.getDiscussionItems().max({i -> i.number})?.number ?: 0
     }
 }
